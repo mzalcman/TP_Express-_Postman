a62a8f77f7df41a00b72942d839699e64fc5845d
ELIEL DAN JAMILIS <49086719@ort>
2026-04-10 11:42:45 -0300

    endpoints matemática b terminados y probados

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { sumar, restar, multiplicar, dividir } from "./modules/matematica.js";
 
 const app = express();
 const port = 5000;
@@ -32,12 +33,50 @@ app.get('/validarfecha/:ano/:mes/:dia', (req, res) => { // EndPoint "/saludar"
 })
 
 
-
-
-
 //
 // Inicio el Server y lo pongo a escuchar.
 //
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+// Endpoints matemática.js
+app.get("/matematica/sumar/:n1/:n2", (req, res) => {
+    const n1 = Number(req.params.n1);
+    const n2 = Number(req.params.n2);
+
+    const resultado = sumar(n1, n2);
+
+    res.status(200).json({ resultado });
+});
+
+app.get("/matematica/restar/:n1/:n2", (req, res) => {
+    const n1 = Number(req.params.n1);
+    const n2 = Number(req.params.n2);
+
+    const resultado = restar(n1, n2);
+
+    res.status(200).json({ resultado });
+});
+
+app.get("/matematica/multiplicar/:n1/:n2", (req, res) => {
+    const n1 = Number(req.params.n1);
+    const n2 = Number(req.params.n2);
+
+    const resultado = multiplicar(n1, n2);
+
+    res.status(200).json({ resultado });
+});
+
+app.get("/matematica/dividir/:n1/:n2", (req, res) => {
+    const n1 = Number(req.params.n1);
+    const n2 = Number(req.params.n2);
+
+    if (n2 === 0) {
+        return res.status(400).json({ error: "El divisor no puede ser cero" });
+    }
+
+    const resultado = dividir(n1, n2);
+
+    res.status(200).json({ resultado });
+});
